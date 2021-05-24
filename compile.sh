ec3ba2a763311c816020207cb808901f3a8029cb
Panuwat <panuwat_jarujareet@hotmail.com>
2021-05-24 22:02:13 +0700

    change volum

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 # https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 
 # find exited container
-docker run --rm -w "/builder" -v "${PWD}:/builder" heroiclabs/nakama-pluginbuilder:3.3.0 build -buildmode=plugin -trimpath -o ./nakama/nakama/data/modules/plugin_code.so
+docker run --rm -w "/builder" -v "${PWD}:/builder" heroiclabs/nakama-pluginbuilder:3.3.0 build -buildmode=plugin -trimpath -o ./nakama/data/modules/plugin_code.so
